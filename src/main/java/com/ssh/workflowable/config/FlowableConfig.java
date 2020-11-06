453524805818f468d175f99abc5f3716b1ab1c03
shensh <shensh>
2020-11-06 16:24:59 +0800

    flowable工作流

---
+++ src/main/java/com/ssh/workflowable/config/FlowableConfig.java
@@ -0,0 +1,21 @@
+package com.ssh.workflowable.config;
+
+import org.flowable.spring.SpringProcessEngineConfiguration;
+import org.flowable.spring.boot.EngineConfigurationConfigurer;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @description 设置样式，解决中文乱
+ * @author: ssh
+ * @Date: 2020/11/6 14:25
+ */
+@Configuration
+public class FlowableConfig implements EngineConfigurationConfigurer<SpringProcessEngineConfiguration> {
+
+    @Override
+    public void configure(SpringProcessEngineConfiguration engineConfiguration) {
+        engineConfiguration.setActivityFontName("宋体");
+        engineConfiguration.setLabelFontName("宋体");
+        engineConfiguration.setAnnotationFontName("宋体");
+    }
+}
---
+++ src/test/java/com/ssh/workflowable/SpringbootWorkFlowableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssh.workflowable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWorkFlowableApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
